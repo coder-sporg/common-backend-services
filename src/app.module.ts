19c17de0f7417dfec53bcaf075c5bb84328bcd97
xuyansheng <xuyansheng@chiansec.com>
2024-03-11 18:56:47 +0800

    添加环境变量配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,50 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
+
 import { UserModule } from './user/user.module';
 import { RolesModule } from './roles/roles.module';
 import { LogsModule } from './logs/logs.module';
 
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
+
 @Module({
-  imports: [UserModule, RolesModule, LogsModule],
+  imports: [
+    ConfigModule.forRoot({
+      // 声明全局模块，其他模块使用时无需在 module 中使用 imports 导入
+      isGlobal: true,
+      // 自定义 env 文件路径
+      envFilePath: envFilePath,
+      // 加载.env文件，并将其中的变量赋值给process.env，默认配置可以读取
+      load: [() => dotenv.config({ path: '.env' })],
+      // 禁止加载环境变量
+      ignoreEnvFile: false,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        // 验证.env文件中的变量是否符合要求
+        // 对数据库类型做校验
+        DB_TYPE: Joi.string().valid('mysql', 'postgres', 'mongodb'),
+        DB_HOST: Joi.alternatives().try(
+          Joi.string().ip(),
+          Joi.string().domain(),
+        ),
+        DB_PORT: Joi.number().default(3306).valid(3306, 3308),
+        DB_DATABASE: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_SYNC: Joi.boolean().default(false),
+        DB_URL: Joi.string().domain(),
+        LOG_ON: Joi.boolean(),
+        LOG_LEVEL: Joi.string(),
+      }),
+    }),
+    UserModule,
+    RolesModule,
+    LogsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { UserService } from './user.service';
+import { ConfigEnum } from 'src/enum/config.enum';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
   getUsers() {
+    const data = this.configService.get(ConfigEnum.DB_DATABASE);
+    console.log('data: ', data); // testdb
+
     return this.userService.getUsers();
   }
 
